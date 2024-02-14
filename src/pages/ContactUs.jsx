b42d860e463a56006db24379724fbc1154c3ba90
Your Name <you@example.com>
2024-02-14 17:29:43 +0500

    Update Changes

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Helmet } from "react-helmet";
+
 import { Container, Box, Typography } from "@mui/material";
 
 import Appbar from "../components/AppBar";
@@ -7,29 +9,34 @@ import Footer from "../components/Footer";
 
 const AboutUs = () => {
   return (
-    <Box>
-      <Appbar />
-      <Container
-        maxWidth="md"
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "90vh",
-          boxShadow:
-            "5px 0px 10px -5px rgba(0, 0, 0, 0.5), -5px 0px 10px -5px rgba(0, 0, 0, 0.5)",
-        }}
-      >
-        <Box>
-          <Typography variant="h4">Contact Us</Typography>
-          <Typography variant="p">
-            For more information, or to work with us, get in touch! We’d love to
-            hear from you.
-          </Typography>
-        </Box>
-      </Container>
-      <Footer />
-    </Box>
+    <>
+      <Helmet>
+        <title>Contact Us</title>
+      </Helmet>
+      <Box>
+        <Appbar />
+        <Container
+          maxWidth="md"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "90vh",
+            boxShadow:
+              "5px 0px 10px -5px rgba(0, 0, 0, 0.5), -5px 0px 10px -5px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <Box>
+            <Typography variant="h4">Contact Us</Typography>
+            <Typography variant="p">
+              For more information, or to work with us, get in touch! We’d love
+              to hear from you.
+            </Typography>
+          </Box>
+        </Container>
+        <Footer />
+      </Box>
+    </>
   );
 };
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Helmet } from "react-helmet";
+
 import Background from "../assets/Tools.jpg";
 
 // import mui
@@ -43,126 +45,131 @@ const LandingPage = () => {
     config: { duration: 500 },
   });
   return (
-    <Box>
-      <Appbar />
-      <Container
-        maxWidth="lg"
-        sx={{
-          boxShadow:
-            "5px 0px 10px -5px rgba(0, 0, 0, 0.5), -5px 0px 10px -5px rgba(0, 0, 0, 0.5)",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Box
+    <>
+      <Helmet>
+        <title>Temp Mails</title>
+      </Helmet>
+      <Box>
+        <Appbar />
+        <Container
+          maxWidth="lg"
           sx={{
-            marginTop: "4rem",
-            paddingTop: "2rem",
-            backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "30rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: { md: "center", sm: "start" },
+            boxShadow:
+              "5px 0px 10px -5px rgba(0, 0, 0, 0.5), -5px 0px 10px -5px rgba(0, 0, 0, 0.5)",
+            paddingBottom: "1rem",
           }}
         >
-          <Typography
-            variant="h3"
-            sx={{
-              color: "white",
-              height: "12rem",
-              fontWeight: "bold",
-              fontSize: "4rem",
-            }}
-          >
-            <ExampleComponent />
-          </Typography>
-        </Box>
-        <Typography
-          variant="h6"
-          textAlign={"center"}
-          marginTop={4}
-          marginBottom={4}
-          sx={{ padding: { xs: "0 2rem", md: "0 4rem" } }}
-        >
-          Introducing our innovative ReactJS tool that empowers users to
-          effortlessly generate random emails, usernames, and names with ease.
-          Seamlessly tailor your user experience by crafting custom usernames,
-          offering unparalleled flexibility. Elevate your productivity and
-          streamline your workflow with our user-friendly interface. Join us
-          today and unlock endless possibilities for personalization and
-          efficiency!
-        </Typography>
-        <Box
-          sx={{
-            width: "100%",
-            height: "300px",
-
-            backgroundImage: `url(${Background})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            position: "relative",
-            zIndex: 1,
-            "&::after": {
-              content: '""',
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              backgroundColor: "rgba(0, 0, 0, 0.5)", // Adjust opacity as needed
-              zIndex: -1,
-            },
-
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <animated.div style={styles}>
-            <Typography
-              sx={{
-                color: "white",
-                fontSize: "30px",
-                fontWeight: "bold",
-                textAlign: "center",
-              }}
-            >
-              Our Tools
-            </Typography>
-          </animated.div>
           <Box
             sx={{
-              marginTop: "2rem",
-              width: "80%",
+              marginTop: "4rem",
+              paddingTop: "2rem",
+              backgroundImage:
+                "url(https://source.unsplash.com/random?wallpapers)",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              height: "30rem",
               display: "flex",
-              flexDirection: { xs: "column", md: "row" },
-              justifyContent: "space-between",
-              alignItems: "center",
+              justifyContent: "center",
+              alignItems: { md: "center", sm: "start" },
             }}
           >
-            {tools.map((tool) => (
-              <Link
-                key={tool.id}
-                to={tool.link}
-                style={{
-                  textDecoration: "none",
+            <Typography
+              variant="h3"
+              sx={{
+                color: "white",
+                height: "12rem",
+                fontWeight: "bold",
+                fontSize: "4rem",
+              }}
+            >
+              <ExampleComponent />
+            </Typography>
+          </Box>
+          <Typography
+            variant="h6"
+            textAlign={"center"}
+            marginTop={4}
+            marginBottom={4}
+            sx={{ padding: { xs: "0 2rem", md: "0 4rem" } }}
+          >
+            Introducing our innovative ReactJS tool that empowers users to
+            effortlessly generate random emails, usernames, and names with ease.
+            Seamlessly tailor your user experience by crafting custom usernames,
+            offering unparalleled flexibility. Elevate your productivity and
+            streamline your workflow with our user-friendly interface. Join us
+            today and unlock endless possibilities for personalization and
+            efficiency!
+          </Typography>
+          <Box
+            sx={{
+              width: "100%",
+              height: "300px",
+
+              backgroundImage: `url(${Background})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              position: "relative",
+              zIndex: 1,
+              "&::after": {
+                content: '""',
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "rgba(0, 0, 0, 0.5)", // Adjust opacity as needed
+                zIndex: -1,
+              },
+
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <animated.div style={styles}>
+              <Typography
+                sx={{
                   color: "white",
-                  borderBottom: "2px solid lightgray",
-                  fontSize: "20px",
+                  fontSize: "30px",
+                  fontWeight: "bold",
+                  textAlign: "center",
                 }}
               >
-                {tool.tool}
-              </Link>
-            ))}
+                Our Tools
+              </Typography>
+            </animated.div>
+            <Box
+              sx={{
+                marginTop: "2rem",
+                width: "80%",
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              {tools.map((tool) => (
+                <Link
+                  key={tool.id}
+                  to={tool.link}
+                  style={{
+                    textDecoration: "none",
+                    color: "white",
+                    borderBottom: "2px solid lightgray",
+                    fontSize: "20px",
+                  }}
+                >
+                  {tool.tool}
+                </Link>
+              ))}
+            </Box>
           </Box>
-        </Box>
-      </Container>
-      <Footer />
-    </Box>
+        </Container>
+        <Footer />
+      </Box>
+    </>
   );
 };
 
