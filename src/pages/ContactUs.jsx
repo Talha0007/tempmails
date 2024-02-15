adbbb9a91247ed2f6fe82614e062466dea276e40
shoaib171 <shoaibrasheed171@gmail.com>
2024-02-15 05:47:36 -0700

    New Changes Done

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,43 +1,41 @@
-import React from "react";
+import React from 'react';
 
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
 
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Box, Typography } from '@mui/material';
 
-import Appbar from "../components/AppBar";
-import Footer from "../components/Footer";
+import Appbar from '../components/AppBar';
+import Footer from '../components/Footer';
 
 const AboutUs = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Contact Us</title>
-      </Helmet>
-      <Box>
-        <Appbar />
-        <Container
-          maxWidth="md"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "90vh",
-            boxShadow:
-              "5px 0px 10px -5px rgba(0, 0, 0, 0.5), -5px 0px 10px -5px rgba(0, 0, 0, 0.5)",
-          }}
-        >
-          <Box>
-            <Typography variant="h4">Contact Us</Typography>
-            <Typography variant="p">
-              For more information, or to work with us, get in touch! We’d love
-              to hear from you.
-            </Typography>
-          </Box>
-        </Container>
-        <Footer />
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<Helmet>
+				<title>Contact Us</title>
+			</Helmet>
+			<Box>
+				<Appbar />
+				<Container
+					maxWidth="md"
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						height: '90vh',
+						boxShadow:
+							'5px 0px 10px -5px rgba(0, 0, 0, 0.5), -5px 0px 10px -5px rgba(0, 0, 0, 0.5)',
+					}}>
+					<Box>
+						<Typography variant="h4">Contact Us</Typography>
+						<Typography variant="p">
+							For more information, or to work with us, get in touch! We’d love to hear from you.
+						</Typography>
+					</Box>
+				</Container>
+				<Footer />
+			</Box>
+		</>
+	);
 };
 
 export default AboutUs;
